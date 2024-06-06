4851c416999d4adcc7e7d889062e305c879dd93b
MAXIMILIANO GARBARINO <47433817@ort>
2024-06-06 10:43:25 -0300

    Iniciando mi proyecto

---
+++ app/contacto/page.js
@@ -0,0 +1,14 @@
+import Titulo from "../components/Titulo";
+
+export default function Flecha() {
+  return (
+    <main >
+      <div>
+        <p>
+        <Titulo texto="Hola Flecha"></Titulo>
+          Bienvenido a mi pagina web junto a mi compañero Raian Kim
+        </p>
+      </div>
+    </main>
+  );
+}
