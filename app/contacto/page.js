67ae5413f4e7d314112459af21cf9d7fdc19665e
YANNICK LERNER <47801170@ort>
2024-07-10 12:15:30 -0300

    agregue view contacto

--- app/contacto/page.js
+++ app/contacto/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import styles from "../page.module.css";
+import styles from "./index.module.css";
 import Head from "next/head";
 import Link from 'next/link';
 
 export default function contacto() {
   return (
     <>
+    
+   
     <nav className={styles.nav}>
       <ul className={styles.ul}>
         <li className={styles.li}>
@@ -22,37 +24,32 @@ export default function contacto() {
         </li>
       </ul>
     </nav>
-    <Head>
-    </Head>
     
-    <main className={styles.main}>
-      
     
+    
+    
+    <main className={styles.principal}>
+                    <div className={styles.contenedorFormulario}>
+                        <h2>Formulario de Contacto</h2>
+                        <form className={styles.formulario}>
+                            <label className={styles.etiqueta} htmlFor="name">Nombre:</label>
+                            <input className={styles.entrada} type="text" id="name" name="name" required />
 
-      <div className={styles.card}>
-      <div className={styles.cardInner}>
-        <div className={styles.cardFront}>
-          <h2 className={styles.cardTitle}>Formulario de Contacto</h2>
-          <form className={styles.form}>
-            <label className={styles.label} htmlFor="name">Nombre:</label>
-            <input className={styles.input} type="text" id="name" name="name" required />
+                            <label className={styles.etiqueta} htmlFor="email">Correo Electrónico:</label>
+                            <input className={styles.entrada} type="email" id="email" name="email" required />
 
-            <label className={styles.label} htmlFor="email">Correo Electrónico:</label>
-            <input className={styles.input} type="email" id="email" name="email" required />
+                            <label className={styles.etiqueta} htmlFor="message">Mensaje:</label>
+                            <textarea className={styles.textarea} id="message" name="message" required></textarea>
 
-            <label className={styles.label} htmlFor="message">Mensaje:</label>
-            <textarea className={styles.textarea} id="message" name="message" required></textarea>
-
-            <button className={styles.button} type="submit">Enviar</button>
-          </form>
-        </div>
-        <div className={styles.cardBack}>
-          <h2 className={styles.cardTitle}>¡Gracias!</h2>
-          <p>Nos pondremos en contacto contigo pronto.</p>
-        </div>
-      </div>
-    </div>
-    </main>
+                            <button className={styles.boton} type="submit">Enviar</button>
+                        </form>
+                    </div>
+                   
+                
+          
+        </main>
+        
+        
     </>
     
   );
--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
     <>
     <Head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"/>
+    
     </Head>
     <nav className={styles.nav}>
       <ul className={styles.ul}>
@@ -28,7 +29,13 @@ export default function Home() {
     </nav>
     <Titulo></Titulo>
     <main className={styles.main}>
-      
+      <div>
+        <h1>Bienvenido!!</h1>
+        <h3 className={styles.inde}>Nuestra plataforma está diseñada para facilitar la vida de los dueños de mascotas y los profesionales del cuidado canino. <br></br>
+         Con Cita para Perros, puedes reservar citas para peluquería, entrenamientos, visitas veterinarias y más, todo desde la comodidad de tu hogar.<br></br> Nos dedicamos a asegurar que cada perro reciba la atención y el cuidado que necesita, cuando lo necesita. Únete a nuestra comunidad y descubre lo fácil y conveniente que es mantener a tu mejor amigo feliz y saludable. ¡Reserva tu cita hoy y dale a tu perro el trato que se merece!
+
+</h3>
+      </div>
     
     </main>
     </>
