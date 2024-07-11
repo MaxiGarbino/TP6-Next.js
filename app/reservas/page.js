eb3b50a97de0fe232b07f38b878881c9dfb26b95
MAXIMILIANO GARBARINO <47433817@ort>
2024-07-11 10:38:13 -0300

    .

--- app/reservas/page.js
+++ app/reservas/page.js
@@ -1,15 +1,14 @@
 "use client";
 import Image from "next/image";
-import Titulo from '../components/Titulo/Titulo.js';
-import Formulario from '../components/Formulario/Formulario.js';
-import Cita from '../components/Cita/Cita.js'
+import Titulo from "../components/Titulo/Titulo.js";
+import Formulario from "../components/Formulario/Formulario.js";
+import Cita from "../components/Cita/Cita.js";
 import styles from "../page.module.css";
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Reservas() {
-  
   const initialCitas = [
     {
       Id: 1,
@@ -36,47 +35,58 @@ export default function Reservas() {
       Sintomas: "No está comiendo",
     },
   ];
-    const [citas, setCitas] = useState(() => {
-      const citasLocalStorage = localStorage.getItem("citas");
-      return citasLocalStorage ? JSON.parse(citasLocalStorage) : initialCitas;
-    });
-  
-    useEffect(() => {
+  const [citas, setCitas] = useState(() => {
+    const citasLocalStorage = localStorage.getItem("citas");
+    return citasLocalStorage ? JSON.parse(citasLocalStorage) : initialCitas;
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
       localStorage.setItem("citas", JSON.stringify(citas));
-    }, [citas]);
+    }
+  }, [citas]);
 
   return (
     <>
-    <nav className={styles.nav}>
-      <ul className={styles.ul}>
-        <li className={styles.li}>
-          <Link href="/" className={styles.link}>Home</Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="/quienes" className={styles.link}>Quiénes Somos</Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="/reservas" className={styles.link}>Reservas</Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="/contacto" className={styles.link}>Contacto</Link>
-        </li>
-      </ul>
-    </nav>
-    <main className={styles.main}>
-      
-    
-      
-    <>
-      {<Titulo/>}
-      <div class="container">
-        <div class="row">
-        <Formulario className='form' citas={citas} setCitas={setCitas}></Formulario>
-        <Cita className='citas' citas={citas} setCitas={setCitas}></Cita>
-        </div>
-      </div>
-    </>
-    </main>
+      <nav className={styles.nav}>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <Link href="/" className={styles.link}>
+              Home
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href="/quienes" className={styles.link}>
+              Quiénes Somos
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href="/reservas" className={styles.link}>
+              Reservas
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href="/contacto" className={styles.link}>
+              Contacto
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main className={styles.main}>
+        <>
+          {<Titulo />}
+          <div class="container">
+            <div class="row">
+              <Formulario
+                className="form"
+                citas={citas}
+                setCitas={setCitas}
+              ></Formulario>
+              <Cita className="citas" citas={citas} setCitas={setCitas}></Cita>
+            </div>
+          </div>
+        </>
+      </main>
     </>
   );
 }
